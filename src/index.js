a08a561994b465026647de3baeb709db2214c1e2
Andrew <andrew@huth.me>
2017-11-05 10:48:15 -0800

    Extract polyfills into their own file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,1 @@
-// Polyfill the Fetch API.
-import "whatwg-fetch"
-// Polyfill promises.
-import Promise from "promise-polyfill";
-if (!window.Promise) {window.Promise = Promise}
+import "./polyfills"
---
+++ src/polyfills.js
@@ -0,0 +1,5 @@
+// Fetch API
+import "whatwg-fetch"
+// Promises
+import Promise from "promise-polyfill";
+if (!window.Promise) { window.Promise = Promise }
