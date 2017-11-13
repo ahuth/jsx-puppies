62f24163fb6b5fc5143aa9d5d061e8df82f3fd4d
Andrew <andrew@huth.me>
2017-11-12 20:56:09 -0800

    Ensure that each "item" shows the hyperlink hand when hovering over it

--- src/components/item.js
+++ src/components/item.js
@@ -6,7 +6,7 @@ import { pupClicked } from "../actions"
 
 export default function Item({ image, index }) {
   return (
-    <div className="card">
+    <div className="card" style="cursor: pointer">
       <div className="card-image" onClick={() => store.dispatch(pupClicked(index))}>
         <figure className="image">
           <img src={image} alt="Dog picture" />
