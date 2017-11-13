8d83af26ba738b23f98dbf586005eab5608796ae
Andrew <andrew@huth.me>
2017-11-12 20:54:25 -0800

    Add a nav bar

--- src/components/app.js
+++ src/components/app.js
@@ -7,14 +7,23 @@ import { createElement } from "../html"
 
 export default function App({ current, loading, images }) {
   return (
-    <div className="section">
-      {loading
-        ? <Loading />
-        : <List images={images} />
-      }
-      {current >= 0 &&
-        <Lightbox image={images[current]} />
-      }
+    <div>
+      <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://www.slack.com">
+            Slack 🐶
+          </a>
+        </div>
+      </nav>
+      <div className="section">
+        {loading
+          ? <Loading />
+          : <List images={images} />
+        }
+        {current >= 0 &&
+          <Lightbox image={images[current]} />
+        }
+      </div>
     </div>
   )
 }
